25689b73e0676494aa771feb91cb0461a3658da8
Rob LaFave <rob.lafave@gmail.com>
2017-11-13 15:41:57 -1000

    About - Removed Team Section

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,7 +23,6 @@ class About extends Component {
 						<div className='row hero-container-text'>
 							<div className='col-sm-12'>
 								<h2>About</h2>
-								<div className='divider-blue mt-30'></div>
 							</div>
 						</div>
 						<div className='row'>
@@ -34,48 +33,6 @@ class About extends Component {
 									Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 								</p>
 								<br />
-							</div>
-						</div>
-					</div>
-				</section>
-
-				<section className='team'>
-					<div className='container'>
-						<div className='row mb-40'>
-							<div className='col-sm-12 mb-30'>
-									<h3>Our Team</h3>
-							</div>
-
-							<div className='col-sm-4'>
-								<article className='team-member-container'>
-									<img src={ 'https://firebasestorage.googleapis.com/v0/b/on-and-on-96714.appspot.com/o/owner.jpeg?alt=media&token=ba660251-3249-472b-bf70-e7ad138aa45c' } width={ '200px' }alt='' className='img-circle team-member-img' />
-									<div className='team-member-name'>
-										<h4>Name</h4>
-									</div>
-									<div className='team-member-title'> Title </div>
-									<hr className='visible-xs-block' />
-								</article>
-							</div>
-
-							<div className='col-sm-4'>
-								<article className='team-member-container'>
-									<img src={ 'https://firebasestorage.googleapis.com/v0/b/on-and-on-96714.appspot.com/o/owner.jpeg?alt=media&token=ba660251-3249-472b-bf70-e7ad138aa45c' } width={ '200px' }alt='' className='img-circle team-member-img' />
-									<div className='team-member-name'>
-											<h4>Name</h4>
-									</div>
-									<div className='team-member-title'> Title </div>
-									<hr className='visible-xs-block' />
-								</article>
-							</div>
-
-							<div className='col-sm-4'>
-								<article className='team-member-container'>
-									<img src={ 'https://firebasestorage.googleapis.com/v0/b/on-and-on-96714.appspot.com/o/owner.jpeg?alt=media&token=ba660251-3249-472b-bf70-e7ad138aa45c' } width={ '200px' }alt='' className='img-circle team-member-img' />
-									<div className='team-member-name'>
-											<h4>Name</h4>
-									</div>
-									<div className='team-member-title'> Title </div>
-								</article>
 							</div>
 						</div>
 					</div>
