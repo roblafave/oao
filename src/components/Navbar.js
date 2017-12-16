4c0259a054804975fa239cb545cac510457a4607
Rob LaFave <rob.lafave@gmail.com>
2017-12-15 16:35:58 -1000

    Navbar - Add Shop Link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -95,11 +95,9 @@ class Navbar extends Component {
 							<li><Link className='navbar-oao-book' to="/book">book</Link></li>
 							<span className='navbar-oao-stroke-book'></span>
 							<li><Link className='navbar-oao-location' to="/location">location</Link></li>
+							<span className='navbar-oao-stroke-location'></span>
+							<li><a href='http://store.on-and-on.co' target='blank' className='navbar-oao-shop'>shop</a></li>
 						</ul>
-
-						<div className='navbar-oao-treatments-stroke'></div>
-						<div className='navbar-oao-book-stroke'></div>
-						<div className='navbar-oao-location-stroke'></div>
 					</div>
 				</div>
 				{this.state.mobnav ?
@@ -134,6 +132,7 @@ const Mobnav = observer(({ close, waveParticipant, avatar, userMatchInfo, user }
 							<li><Link onClick={close} to="/treatments">treatments</Link></li>
 							<li><Link onClick={close} to="/book">book</Link></li>
 							<li><Link onClick={close} to="/location">location</Link></li>
+							<li><Link onClick={close} to="/shop">shop</Link></li>
 							<li><Link onClick={close} to="/welcome">signup waiver</Link></li>
 						</ul>
 							{appState.userMeta.emailauth &&
