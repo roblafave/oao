55a3896832c0447c9cee69ac6352913910ca3cb7
Rob LaFave <rob.lafave@gmail.com>
2018-03-13 12:40:28 -1000

    Revised Store Hours and Map Link

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -36,20 +36,22 @@ class Location extends Component {
 									<b>h &nbsp; a &nbsp; n &nbsp; a &nbsp; l &nbsp; e &nbsp; i</b>
 								</div>
 								<br/>
-								<a href='https://www.google.com/maps/place/5-5161+Kuhio+Hwy+%23210,+Hanalei,+HI+96714/@22.202636,-159.4987253,17z/data=!3m1!4b1!4m5!3m4!1s0x7c06fac6d2479425:0xf2622cf94b3bc177!8m2!3d22.202631!4d-159.4965313' target='_blank'>5-5161 kuhio hwy suite 210
+								<a href='https://www.google.com/maps/place/ON+%26+ON/@22.202636,-159.4987253,17z/data=!3m1!4b1!4m5!3m4!1s0x7c06fac13259d821:0x770dfbfc59b3e02c!8m2!3d22.202631!4d-159.4965313' target='_blank'>5-5161 kuhio hwy suite 210
 								<br/>
 								hanalei, hi 96714</a>
 								<br/><br/>
-								9:00am - 7:00pm
+								Mon – Sat, 9a – 6p
 								<br/>
-								open 7 days
+								Sun, CLOSED*
+								<br/><br/>
+								<i>*special events & workshops TBA</i>
 
 								<div className='row'>
 								<img src={ starburst } width={ '200px' }alt='ON & ON Starburst Graphic' className='location-starburst-img hidden-xs hidden-sm' />
 								</div>
 							</div>
 							<div className='col-sm-6 mt-30'>
-								<a href='https://www.google.com/maps/place/5-5161+Kuhio+Hwy+%23210,+Hanalei,+HI+96714/@22.202636,-159.4987253,17z/data=!3m1!4b1!4m5!3m4!1s0x7c06fac6d2479425:0xf2622cf94b3bc177!8m2!3d22.202631!4d-159.4965313' target='_blank'><img src={ map } style={{ width:'100%' }} alt="services" /></a>
+								<a href='https://www.google.com/maps/place/ON+%26+ON/@22.202636,-159.4987253,17z/data=!3m1!4b1!4m5!3m4!1s0x7c06fac13259d821:0x770dfbfc59b3e02c!8m2!3d22.202631!4d-159.4965313' target='_blank'><img src={ map } style={{ width:'100%' }} alt="services" /></a>
 							</div>
 						</div>
 
